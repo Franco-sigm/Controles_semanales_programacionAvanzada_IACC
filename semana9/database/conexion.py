b9f1f7d3574d4bc3d64c4c9115b2c8d5297168c5
Franco-sigm <francoherrera.30@outlook.com>
2025-08-01 19:35:07 -0400

    Update conexion.py

--- semana9/database/conexion.py
+++ semana9/database/conexion.py
@@ -5,7 +5,8 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="WEYDUjdhdcjsl1903Q###",
-        database="saludtotal",
-        port=3307
+        password="",
+        database="saludtotal"
+        
     )
+
